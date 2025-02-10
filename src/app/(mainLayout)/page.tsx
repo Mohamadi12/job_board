f20f4e56965991e37bf72de57feff160794a1317
Nana Mohamadi <adilnana78@gmail.com>
2025-02-10 20:42:05 +0000

    Filtrage

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,19 +1,41 @@
-import JobFilter from '@/components/general/JobFilter'
-import JobListings from '@/components/general/JobListings'
-import JobListingsLoading from '@/components/general/JobListingsLoading'
-import React, { Suspense } from 'react'
+import JobFilter from "@/components/general/JobFilter";
+import JobListings from "@/components/general/JobListings";
+import JobListingsLoading from "@/components/general/JobListingsLoading";
+import React, { Suspense } from "react";
 
-const Home = () => {
+type SearchParamsProps = {
+  searchParams: Promise<{
+    page?: string;
+    jobTypes?: string;
+    location?: string;
+  }>;
+};
+
+const Home = async ({ searchParams }: SearchParamsProps) => {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
+  const jobTypes = params.jobTypes?.split(",") || [];
+  const location = params.location || "";
+
+  // Create a composite key from all filter parameters
+  const filterKey = `page=${currentPage};types=${jobTypes.join(
+    ","
+  )};location=${location}`;
+
   return (
     <div className="grid grid-cols-3 gap-8">
       <JobFilter />
       <div className="col-span-2 flex flex-col gap-6">
-        <Suspense fallback={<JobListingsLoading />}>
-        <JobListings />
+        <Suspense key={filterKey} fallback={<JobListingsLoading />}>
+          <JobListings
+            currentPage={currentPage}
+            jobTypes={jobTypes}
+            location={location}
+          />
         </Suspense>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
